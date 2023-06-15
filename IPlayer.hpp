7ed1c31b0c21d65b30223b98af390f32864d9e1c
Nathan Wong <48112488+lightbulbss@users.noreply.github.com>
2023-06-15 01:19:22 -0700

    Update IPlayer.hpp

--- IPlayer.hpp
+++ IPlayer.hpp
@@ -9,8 +9,6 @@ class IPlayer {
 
     virtual int stopSong() = 0;
 
-    //virtual ISongData* getSongDetails(int index) = 0; // For 4-25, this method is not required for turn in but required later
-
     virtual int addSongToList(std::string song_path) = 0; // add a song to the list along with a unique id (based on ISongData)
 
 
